1e045fbc3697ea61dc68495ed420e4ab6595a1f2
Ruslan Izhitsky <ksilobait@gmail.com>
2020-01-19 22:58:12 +0700

    array: template typization and custom nodes
    Node.arrayListOfChildren instead of Object[]

--- src/main/java/persistent/PersistentArray.java
+++ src/main/java/persistent/PersistentArray.java
@@ -1,14 +1,50 @@
 package persistent;
 
-public class PersistentArray {
+import java.util.ArrayList;
 
-    private final Object[] root;
+public class PersistentArray<T> {
+
+    private final Node root;
     private final int branchingFactor;
     private final int depth;
     private final int base; //BF ^ (depth - 1)
     private final int size;
 
-    public PersistentArray(Object[] root, int branchingFactor, int depth, int base, int size) {
+    private class Node {
+
+        ArrayList<Node> children;
+        T data;
+
+        Node() {
+            this.data = null;
+            this.children = new ArrayList<>();
+            for (int i = 0; i < branchingFactor; i++) {
+                this.children.add(null);
+            }
+        }
+
+        Node(T data) {
+            this.data = data;
+            this.children = null;
+        }
+
+        Node get(int i) {
+            if (this.children.size() <= i) {
+                return null;
+            }
+            return this.children.get(i);
+        }
+
+        void set(int i, Node e) {
+            /*if (this.children.size() <= i) {
+                this.children.add(e);
+            } else {*/
+            this.children.set(i, e);
+            // }
+        }
+    }
+
+    private PersistentArray(Node root, int branchingFactor, int depth, int base, int size) {
         this.root = root;
         this.branchingFactor = branchingFactor;
         this.depth = depth;
@@ -16,61 +52,61 @@ public class PersistentArray {
         this.size = size;
     }
 
-    public PersistentArray(Object data, int powerOfBranchingFactor) {
+    public PersistentArray(T data, int powerOfBranchingFactor) {
         int branchingFactor = 1;
         for (int i = 0; i < powerOfBranchingFactor; i++) {
             branchingFactor *= 2;
         }
 
-        this.root = new Object[branchingFactor];
-        this.root[0] = data;
         this.branchingFactor = branchingFactor;
+        this.root = new Node();
+        this.root.set(0, new Node(data));
         this.depth = 1;
         this.base = 1;
         this.size = 1;
     }
 
     //Replaces the element (to be returned) at the specified position in this list with the specified element
-    public PersistentArray set(int index, Object data) {
+    public PersistentArray<T> set(int index, T data) {
         int newSize = this.size;
-        if (newSize == index) { //when adding to the end ?TODO?
+        if (newSize == index) {
             newSize++;
         }
 
-        Object[] newRoot = new Object[branchingFactor]; //TODO 2 -> BF
+        Node newRoot = new Node();
 
-        Object[] currentNode = this.root;
-        Object[] currentNewNode = newRoot;
+        Node currentNode = this.root;
+        Node currentNewNode = newRoot;
 
-        for (int b = base; b > 1; b = b / branchingFactor) { //TODO b=b/2 -> b=b/BF
+        for (int b = base; b > 1; b = b / branchingFactor) {
             int nextBranch = index / b;
-            currentNewNode[nextBranch] = new Object[branchingFactor]; //TODO 2 -> BF
+            currentNewNode.set(nextBranch, new Node());
 
             for (int anotherBranch = 0; anotherBranch < branchingFactor; anotherBranch++) {
                 if (anotherBranch == nextBranch) {
                     continue;
                 }
-                currentNewNode[anotherBranch] = currentNode[anotherBranch];
+                currentNewNode.set(anotherBranch, currentNode.get(anotherBranch));
             }
 
             //down
-            currentNode = (Object[]) currentNode[nextBranch];
-            currentNewNode = (Object[]) currentNewNode[nextBranch];
+            currentNode = currentNode.get(nextBranch);
+            currentNewNode = currentNewNode.get(nextBranch);
             index = index % b;
         }
 
-        currentNewNode[index] = data;
+        currentNewNode.set(index, new Node(data));
         for (int i = 0; i < branchingFactor; i++) {
             if (i == index) {
                 continue;
             }
-            currentNewNode[i] = currentNode[i];
+            currentNewNode.set(i, currentNode.get(i));
         }
 
-        return new PersistentArray(newRoot, this.branchingFactor, this.depth, this.base, newSize);
+        return new PersistentArray<>(newRoot, this.branchingFactor, this.depth, this.base, newSize);
     }
 
-    public PersistentArray add(Object data) {
+    public PersistentArray<T> add(T data) {
         //there's still space in the latest element
         if (this.size % branchingFactor != 0) {
             return set(this.size, data);
@@ -78,114 +114,118 @@ public class PersistentArray {
 
         //there's still space for the new data
         if (this.base * branchingFactor > this.size) {
-            Object[] newRoot = new Object[branchingFactor];
+            Node newRoot = new Node();
 
-            Object[] currentNode = this.root;
-            Object[] currentNewNode = newRoot;
+            Node currentNode = this.root;
+            Node currentNewNode = newRoot;
 
             int index = this.size;
             int b;
             for (b = base; b > 0; b = b / branchingFactor) {
                 int nextBranch = index / b;
-                currentNewNode[nextBranch] = new Object[branchingFactor];
+                currentNewNode.set(nextBranch, new Node());
 
                 for (int anotherBranch = 0; anotherBranch < branchingFactor; anotherBranch++) {
                     if (anotherBranch == nextBranch) {
                         continue;
                     }
-                    currentNewNode[anotherBranch] = currentNode[anotherBranch];
+                    currentNewNode.set(anotherBranch, currentNode.get(anotherBranch));
                 }
 
                 //down
-                currentNewNode = (Object[]) currentNewNode[nextBranch];
+                currentNewNode = currentNewNode.get(nextBranch);
                 index = index % b;
 
-                if (currentNode[nextBranch] == null) {
+                if (currentNode.get(nextBranch) == null) {
                     b = b / branchingFactor;
                     break;
                 } else {
-                    currentNode = (Object[]) currentNode[nextBranch];
+                    currentNode = currentNode.get(nextBranch);
                 }
             }
             while (b > 1) {
-                currentNewNode[0] = new Object[branchingFactor];
-                currentNewNode = (Object[]) currentNewNode[0];
+                currentNewNode.set(0, new Node());
+                currentNewNode = currentNewNode.get(0);
                 index = index % b;
                 b = b / branchingFactor;
             }
-            currentNewNode[0] = data;
+            currentNewNode.set(0, new Node(data));
 
-            return new PersistentArray(newRoot, this.branchingFactor, this.depth, this.base,
+            return new PersistentArray<T>(newRoot, this.branchingFactor, this.depth, this.base,
                 this.size + 1);
         }
 
         //root overflow
-        Object[] newRoot = new Object[branchingFactor];
-        newRoot[0] = this.root;
-        newRoot[1] = new Object[branchingFactor];
+        Node newRoot = new Node();
+        newRoot.set(0, this.root);
+        newRoot.set(1, new Node());
         //newRoot[2..]=null
-        Object[] currentNewNode = (Object[]) newRoot[1];
+        Node currentNewNode = newRoot.get(1);
 
         int b = base;
         while (b > 1) {
-            currentNewNode[0] = new Object[branchingFactor];
-            currentNewNode = (Object[]) currentNewNode[0];
+            currentNewNode.set(0, new Node());
+            currentNewNode = currentNewNode.get(0);
             b = b / branchingFactor;
         }
-        currentNewNode[0] = data;
+        currentNewNode.set(0, new Node(data));
 
-        return new PersistentArray(newRoot, this.branchingFactor, this.depth + 1,
+        return new PersistentArray<T>(newRoot, this.branchingFactor, this.depth + 1,
             this.base * branchingFactor, this.size + 1);
     }
 
-    public PersistentArray pop() {
+    public PersistentArray<T> pop() {
         //the latest element won't become empty
         int index = this.size - 1;
-        Object[] currentNode = this.root;
-        Object[] newRoot = new Object[branchingFactor];
-        Object[] currentNewNode = newRoot;
+        Node newRoot = new Node();
+
+        Node currentNode = this.root;
+        Node currentNewNode = newRoot;
 
         for (int b = base; b > 1; b = b / branchingFactor) {
             int nextBranch = index / b;
-            currentNewNode[nextBranch] = new Object[branchingFactor];
+            currentNewNode.set(nextBranch, new Node());
 
             for (int anotherBranch = 0; anotherBranch < branchingFactor; anotherBranch++) {
                 if (anotherBranch == nextBranch) {
                     continue;
                 }
-                currentNewNode[anotherBranch] = currentNode[anotherBranch];
+                currentNewNode.set(anotherBranch, currentNode.get(anotherBranch));
             }
 
             //down
-            currentNode = (Object[]) currentNode[nextBranch];
-            currentNewNode = (Object[]) currentNewNode[nextBranch];
+            currentNode = currentNode.get(nextBranch);
+            currentNewNode = currentNewNode.get(nextBranch);
             index = index % b;
         }
 
         for (int i = 0; i < branchingFactor && i < index; i++) {
-            currentNewNode[i] = currentNode[i];
+            currentNewNode.set(i, currentNode.get(i));
         }
-        currentNewNode[index] = null;
+        currentNewNode.set(index, null);
 
-        return new PersistentArray(newRoot, this.branchingFactor, this.depth, this.base,
+        return new PersistentArray<T>(newRoot, this.branchingFactor, this.depth, this.base,
             this.size - 1);
     }
 
-    private String toStringHelper(Object[] node, int curDepth) {
+    private String toStringHelper(Node node, int curDepth) {
+        if (node.data != null) {
+            return node.data.toString();
+        }
         StringBuilder outString = new StringBuilder();
         for (int i = 0; i < branchingFactor; i++) {
-            if (node[i] == null) {
+            if (node.get(i) == null) {
                 outString.append("_");
                 if (i + 1 != branchingFactor) {
                     outString.append(", ");
                 }
                 //break;
             } else {
-                if (curDepth == 1) {
-                    outString.append(node[i].toString());
+                if (curDepth == 0) {
+                    outString.append(node.get(i).toString());
 
                 } else {
-                    outString.append(toStringHelper((Object[]) node[i], curDepth - 1));
+                    outString.append(toStringHelper(node.get(i), curDepth - 1));
                 }
                 if (i + 1 != branchingFactor) {
                     outString.append(", ");
